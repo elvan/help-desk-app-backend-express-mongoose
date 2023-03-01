5358edd7ce7f6d6c21e20fe69892a0e381ed8d1c
Elvan Hidayat <elvanhd@gmail.com>
2023-03-02 03:06:51 +0700

    login and create jwt

--- controllers/user/loginUserController.js
+++ controllers/user/loginUserController.js
@@ -1,14 +1,26 @@
+const bcrypt = require('bcryptjs');
 const expressAsyncHandler = require('express-async-handler');
 
-const loginUserController = expressAsyncHandler(async (req, res) => {
-  const { email } = req.body;
+const generateToken = require('../../helpers/generateToken');
+const User = require('../../models/userModel');
 
-  res.status(200).json({
-    message: 'Login successful',
-    data: {
-      email,
-    },
-  });
+const loginUserController = expressAsyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  // Check user and passwords match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid credentials');
+  }
 });
 
 module.exports = loginUserController;
